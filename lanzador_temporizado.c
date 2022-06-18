638bc8dbcc09d0d128ea258d44a94defbf84223a
Silvia Arenales <91908502+sarenales@users.noreply.github.com>
2022-06-18 17:16:15 +0200

    Update lanzador_temporizado.c

--- lanzador_temporizado.c
+++ lanzador_temporizado.c
@@ -14,15 +14,10 @@
 
 int main(int argc, char *argv[]) { 
 
-	int clockid, timer, i, child, childp, id, cont;
-	time_t t0, t1,t2;
+	int clockid, timer, i, childid, childp, id, cont, status;
+	time_t t1,t2;
 
-	// Empieza a contar el timer.
-	t0 = time(NULL);
-	time(&t0);
-	
-	
-	
+	cont = 0;
 	
 	// Control 1: Numero de parametros dentro de lo establecido.
 	if (argc < 3 || argc > 5)
@@ -35,85 +30,65 @@ int main(int argc, char *argv[]) {
 		error("No se ha introducido ningun numero en el segundo parametro.");	
 	
 	
+	if((clockid = fork())==0){
+		execlp("./clock", "./clock", argv[1], NULL);
+		perror("execlp clock.");
+	}else if ((clockid = fork())==-1) 
+		perror("Error fork clockid.");
 	
-	switch(clockid=fork()){
-		case -1: 
-			error("fork child clock");
-			break;
-			
-			
-		case 0: /*hijo*/
-			execlp("./clock", "./clock", timer, NULL);
-			break;
-			
-			
-		case 1: /* padre*/
-			// Encargado de crear los procesos concurrentes.
-			for (i = 2; i < argc ; i++)
-			{		
-				// Se crea otro proceso hijo para luego poder escribir, tras haber terminado.
-				if((childp = fork()) == 0){
-					printf("Programa numero %d (%s).---------------------\n ", i-1,argv[i]);
-					execvp(argv[i], &(argv[i]));
-					error("Error execvp. ");
-				}
-				else if (childp == -1 ) error ("Fork childp");
-				
-				printf("Proceso subhijo : %d \n", childp);
-				
-				while(wait(NULL) != childp);
-				
-				printf("Programa numero %d (%s)terminado.---------------------\n ", i-1,argv[i]);
-				
-				cont ++; 
-				
-				kill(childp, SIGKILL);
+	printf("--clock-process: %d\n ", clockid);
+	
+	if((childid = fork()) == 0 ){
+		for (i = 2; i < argc ; i++){	
+		
+			if((childp = fork()) == 0){
+				printf("Programa numero %d (%s).---------------------\n ", i-1,argv[i]);
+				execvp(argv[i], &(argv[i]));
+				error("Error execvp. ");
 			}
-			while(wait(NULL) != child);
+			else if (childp == -1 )
+				error ("Fork childp");
+				
+			while(wait(NULL) != childp);
+				
+			printf("Programa numero %d (%s)terminado.---------------------\n ", i-1,argv[i]);
+			printf("\n");
 			
-			printf("%d comando terminado \n", child);
-			printf(" \n");
-			printf(" \n");
-
+			cont++;
 			
-			break;
-		
-		
-		
-	}	
+			kill(childp, SIGKILL);
+		}
+			
+		// while(wait(NULL) != child);
+			
+		printf("Commands finished first, stop clock. \n");
+	}else if((childid = fork()) == -1 )
+		perror("Error fork childid.");
 	
 	
-	// while(wait(NULL) != child);	
+	t1 = time(NULL);
 	
-	id = wait(NULL);
+	id = wait(NULL);	
 	
-	
-    if(id == clockid)
+    if(id == childid) 	/* child finishes first; stop clock */
     { 
-		printf("id: %d \n", id);
-        printf("El reloj termina antes que los procesos.\n");	
-		printf("%d procesos realizados de %d.\n", cont, argc-2);
-		time(&t2);
-		t2 = time(NULL);
-		time(&t2);
-		printf("------------------------------------ \n");
-		printf("------------------------------------ \n");
-		printf("El tiempo total es de %d segundos. \n", t2 - t0); 
-		exit(0);
-    }else
-    { 	
-		printf("id: %d \n", id);
-		printf("Los hijos terminan antes del tiempo establecido. \n");
-		time(&t2);
-		t2 = time(NULL);
-		time(&t2);
-		printf("------------------------------------ \n");
-		printf("------------------------------------ \n");
-		printf("El tiempo total es de %d segundos. \n", t2 - t0);
 		kill(clockid, SIGKILL);
-		exit(0);
+		wait(NULL);
+		printf("child finishes first; stop clock . \n");
+		status = 0;
+    }else				/* clock finishes first; force child to finish */
+    { 	
+		kill(childid, SIGKILL);
+		wait(NULL);
+		printf("%d procesos realizados de %d.\n", cont, argc-2);
+		printf("--overtime. Clock finishes first. \n");
+		status = 1;
     }	
 	
-	return (0);
+	t2 = time(NULL);
+	
+	printf("--child time: %d\n", t2-t1);
+	
+	exit(status);
 	
 }
