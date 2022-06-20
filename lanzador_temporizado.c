4672723ee47051f70d7a8e738f6073013fe53612
Silvia Arenales <91908502+sarenales@users.noreply.github.com>
2022-06-20 15:20:11 +0200

    Update lanzador_temporizado.c

--- lanzador_temporizado.c
+++ lanzador_temporizado.c
@@ -8,7 +8,7 @@
 #include <string.h>
 #include <fcntl.h>
 
-#define error(a){perror(a); exit(1);};
+#define error(a){perror(a); _exit(-1);};
 
 // a 10 dat ls ps sleep20 who
 
@@ -16,12 +16,14 @@ int main(int argc, char *argv[]) {
 
 	int clockid, timer, i, childid, childp, id, cont, status;
 	time_t t1,t2;
+	char argv1[80];
 
+	status = 0;
 	cont = 0;
 	
 	// Control 1: Numero de parametros dentro de lo establecido.
-	if (argc < 3 || argc > 5)
-		error("Nimero de parametros incorrectos. ");
+	if (argc < 3 || argc > 7)
+		error("Numero de parametros incorrectos. ");
 	
 	
 	// Control 2: El segundo parametro debe de ser un numero.
@@ -29,66 +31,56 @@ int main(int argc, char *argv[]) {
 	if(timer <= 0 )
 		error("No se ha introducido ningun numero en el segundo parametro.");	
 	
+	t1 = time(NULL);
 	
-	if((clockid = fork())==0){
+	if((clockid = fork())== 0){
 		execlp("./clock", "./clock", argv[1], NULL);
 		perror("execlp clock.");
-	}else if ((clockid = fork())==-1) 
+	}else if(clockid == -1)
 		perror("Error fork clockid.");
 	
-	printf("--clock-process: %d\n ", clockid);
-	
-	if((childid = fork()) == 0 ){
-		for (i = 2; i < argc ; i++){	
-		
+	if((childid = fork()) == 0){
+		for (i = 2; i < argc ; i++){			
 			if((childp = fork()) == 0){
-				printf("Programa numero %d (%s).---------------------\n ", i-1,argv[i]);
-				execvp(argv[i], &(argv[i]));
+				execlp(argv[i], argv[i], NULL);
 				error("Error execvp. ");
 			}
 			else if (childp == -1 )
 				error ("Fork childp");
-				
-			while(wait(NULL) != childp);
-				
-			printf("Programa numero %d (%s)terminado.---------------------\n ", i-1,argv[i]);
-			printf("\n");
-			
-			cont++;
-			
-			kill(childp, SIGKILL);
+
+			// printf("Proceso (%s) terminado. \n", argv[i]);	
 		}
-			
-		// while(wait(NULL) != child);
-			
-		printf("Commands finished first, stop clock. \n");
-	}else if((childid = fork()) == -1 )
+		for (i = 2; i < argc ; i++){		
+			id = wait(NULL);
+			printf("proceso (%d) terminado. \n", id);
+		}
+	}else if(childid == -1)
 		perror("Error fork childid.");
 	
 	
-	t1 = time(NULL);
+	id = wait(NULL);
 	
-	id = wait(NULL);	
 	
-    if(id == childid) 	/* child finishes first; stop clock */
+	
+    if(id == clockid) 	/* clock finishes first; force child to finish */
     { 
-		kill(clockid, SIGKILL);
-		wait(NULL);
-		printf("child finishes first; stop clock . \n");
-		status = 0;
-    }else				/* clock finishes first; force child to finish */
-    { 	
 		kill(childid, SIGKILL);
 		wait(NULL);
-		printf("%d procesos realizados de %d.\n", cont, argc-2);
+		
 		printf("--overtime. Clock finishes first. \n");
 		status = 1;
+		
+    }else				/* child finishes first; stop clock */
+    { 	
+		kill(clockid, SIGKILL);
+		wait(NULL);
+		
+		status = 0;
+		t2 = time(NULL);
+		printf("--TIME : (%d)s \n", t2-t1);
     }	
 	
-	t2 = time(NULL);
-	
-	printf("--child time: %d\n", t2-t1);
-	
 	exit(status);
+	return (status);
 	
 }
